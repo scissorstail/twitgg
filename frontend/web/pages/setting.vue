d3db6ccd53fe3e473f0981bda46d257119373927
scissorstail <scissorstail@gmail.com>
2022-06-01 18:09:23 +0900

    setting 화면 추가

---
+++ frontend/web/pages/setting.vue
@@ -0,0 +1,31 @@
+<script setup>
+definePageMeta({
+  middleware: ['auth']
+})
+
+const user = await useUser()
+
+const refresh = () => {
+  refreshUser()
+}
+</script>
+
+<template>
+  <div>
+    <h1>Setting page</h1>
+
+    <div>
+      <NuxtLink to="/">
+        메인
+      </NuxtLink>
+    </div>
+    <div>
+      {{ user.user_name }}
+    </div>
+    <div>
+      <button @click="refresh">
+        사용자 정보 새로고침
+      </button>
+    </div>
+  </div>
+</template>
