bf732cfe6ddee2c0d85ec2be0eeb362cd5fb6816
scissorstail <scissorstail@gmail.com>
2022-06-03 23:32:16 +0900

    유저 테이블 변경

--- backend/api/src/storage/pg/schema.sql
+++ backend/api/src/storage/pg/schema.sql
@@ -6,13 +6,16 @@
 
 CREATE TABLE public.users (
 	user_no serial4 NOT NULL,
-	user_id varchar NULL,
-	user_pw varchar NULL,
-	user_name varchar NULL,
-	user_provider varchar NULL,
+	user_id varchar NOT NULL,
+	user_pw varchar NOT NULL,
+	user_name varchar NOT NULL,
+	user_nick varchar NOT NULL,
+	user_provider varchar NOT NULL,
 	user_provider_info jsonb NOT NULL DEFAULT '{}'::jsonb,
 	created_dt timestamptz NOT NULL DEFAULT now(),
 	updated_dt timestamptz NULL,
-	state int4 NULL,
-	CONSTRAINT users_pk PRIMARY KEY (user_no)
+	state int4 NOT NULL DEFAULT 1,
+	CONSTRAINT users_pk PRIMARY KEY (user_no),
+	CONSTRAINT users_un UNIQUE (user_id),
+	CONSTRAINT users_un_1 UNIQUE (user_name)
 );
