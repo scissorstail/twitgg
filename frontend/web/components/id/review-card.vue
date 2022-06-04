9446aa68b6d2a033f453211ba7cab631bdc1b536
scissorstail <scissorstail@gmail.com>
2022-06-04 21:11:53 +0900

    리뷰 관련 카드 수정

--- frontend/web/components/id/review-card.vue
+++ frontend/web/components/id/review-card.vue
@@ -1,4 +1,6 @@
 <script setup>
+import dayjs from 'dayjs'
+
 const props = defineProps({
   review: {
     type: Object,
@@ -8,32 +10,48 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="card ring-1 ring-black/5 p-5 mb-4 bg-base-100">
-    <div class="flex mb-2">
+  <div class="card ring-1 ring-black/5 p-3 mb-4 bg-base-100">
+    <div class="flex items-center mb-1 ">
+      <div class="pl-2 py-1 w-32 text-lg font-extrabold" style="pointer-events: none;">
+        <span v-if="props.review.rv_positive === 1" class="text-blue-500">추천</span>
+        <span v-else-if="props.review.rv_positive === 0" class="text-red-500">비추천</span>
+      </div>
+      <!-- 추후 메뉴배치 -->
+      <!-- <div class="ml-auto">
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-square">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" /></svg>
+          </label>
+          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>메뉴명</a></li>
+          </ul>
+        </div>
+      </div> -->
+    </div>
+
+    <p class="text-base-content text-md text-opacity-80 px-3 py-1 mb-1">
+      {{ props.review.rv_content }}
+    </p>
+
+    <div class="divider mb-1" />
+
+    <div class="flex">
       <div class="flex items-center space-x-2">
-        <label class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
-            <img :src="props.review.user_profile_image_url">
-          </div>
-        </label>
-        <div class="text-sm text-base-content/70">
+        <NuxtLink :to="`/${props.review.user_name}`">
+          <label class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img :src="props.review.user_profile_image_url">
+            </div>
+          </label>
+        </NuxtLink>
+        <div class="text-sm">
           <div>
             @{{ props.review.user_name }}
           </div>
+          <div class="text-xs text-base-content/70">
+            게시 일시: {{ dayjs(props.review.created_dt).format('YYYY년 MM월 DD일') }} {{ props.review.updated_dt ? '(수정됨)' : '' }}
+          </div>
         </div>
-      </div>
-      <div class="flex flex-1 items-center space-x-2">
-        <div class="ml-auto text-md font-extrabold">
-          <span v-if="props.review.rv_positive === 1" class="text-blue-500">추천</span>
-          <span v-else-if="props.review.rv_positive === 0" class="text-red-500">비추천</span>
-        </div>
-      </div>
-    </div>
-    <div class="flex w-full">
-      <div class="w-full">
-        <p class="text-base-content text-md text-opacity-80">
-          {{ props.review.rv_content }}
-        </p>
       </div>
     </div>
   </div>
--- frontend/web/components/id/review-write-card.vue
+++ frontend/web/components/id/review-write-card.vue
@@ -1,4 +1,6 @@
 <script setup>
+import dayjs from 'dayjs'
+
 const props = defineProps({
   user: {
     type: Object,
@@ -10,8 +12,9 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(['create', 'update'])
+const emit = defineEmits(['change'])
 
+const isRunning = ref(false)
 const myReview = ref(null)
 
 const rvContent = ref(null)
@@ -28,37 +31,82 @@ const loadMyReview = async () => {
   })
 
   myReview.value = data
-  if (myReview.value) {
-    rvPositive.value = myReview.value.rv_positive
-    rvContent.value = myReview.value.rv_content
-  }
+  rvPositive.value = myReview.value ? myReview.value.rv_positive : null
+  rvContent.value = myReview.value ? myReview.value.rv_content : null
 }
 
 const create = async () => {
-  await useApi('/user/v1/review', {
-    method: 'POST',
-    params: {
-      rv_content: rvContent.value,
-      rv_positive: rvPositive.value,
-      rv_user_no: props.info.user_no
-    }
-  })
+  if (isRunning.value) {
+    return
+  }
 
-  loadMyReview()
-  emit('create')
+  try {
+    isRunning.value = true
+
+    await useApi('/user/v1/review', {
+      method: 'POST',
+      params: {
+        rv_content: rvContent.value,
+        rv_positive: rvPositive.value,
+        rv_user_no: props.info.user_no
+      }
+    })
+
+    await loadMyReview()
+    emit('change')
+  } finally {
+    setTimeout(() => {
+      isRunning.value = false
+    }, 250)
+  }
 }
 
 const update = async () => {
-  await useApi(`/user/v1/review/${myReview.value.rv_no}`, {
-    method: 'PUT',
-    params: {
-      rv_content: rvContent.value,
-      rv_positive: rvPositive.value
-    }
-  })
+  if (isRunning.value) {
+    return
+  }
 
-  loadMyReview()
-  emit('update')
+  try {
+    isRunning.value = true
+
+    await useApi(`/user/v1/review/${myReview.value.rv_no}`, {
+      method: 'PUT',
+      params: {
+        rv_content: rvContent.value,
+        rv_positive: rvPositive.value
+      }
+    })
+
+    await loadMyReview()
+    emit('change')
+  } finally {
+    setTimeout(() => {
+      isRunning.value = false
+    }, 250)
+  }
+}
+
+const remove = async () => {
+  if (isRunning.value) {
+    return
+  }
+
+  try {
+    isRunning.value = true
+    await useApi(`/user/v1/review/${myReview.value.rv_no}`, {
+      method: 'PUT',
+      params: {
+        state: 0
+      }
+    })
+
+    await loadMyReview()
+    emit('change')
+  } finally {
+    setTimeout(() => {
+      isRunning.value = false
+    }, 250)
+  }
 }
 
 await loadMyReview()
@@ -68,15 +116,29 @@ await loadMyReview()
   <div class="card p-3 mb-4 bg-base-100">
     <div class="flex">
       <div class="flex items-center space-x-2">
-        <label class="avatar">
+        <label class="btn btn-ghost btn-circle avatar" style="pointer-events: none;">
           <div class="w-10 rounded-full">
             <img :src="props.user.user_profile_image_url">
           </div>
         </label>
-        <div class="text-sm text-base-content/70">
+        <div class="text-sm">
           <div>
             @{{ props.user.user_name }}
           </div>
+          <div v-if="myReview" class="text-xs text-base-content/70">
+            게시 일시: {{ dayjs(myReview.created_dt).format('YYYY년 MM월 DD일') }} {{ myReview.updated_dt ? '(수정됨)' : '' }}
+          </div>
+        </div>
+      </div>
+
+      <div v-if="myReview" class="flex ml-auto">
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-square">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" /></svg>
+          </label>
+          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a @click="remove">삭제하기</a></li>
+          </ul>
         </div>
       </div>
     </div>
@@ -96,7 +158,11 @@ await loadMyReview()
       </div>
     </div>
     <div class="flex mb-4 space-x-2">
-      <button class="btn btn-sm w-32 btn-outline border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white hover:border-blue-500" @click="rvPositive = 1">
+      <button
+        class="btn btn-sm w-32 btn-outline border-blue-500 hover:bg-blue-500 hover:text-white hover:border-blue-500"
+        :class="rvPositive === 1 ? ['bg-blue-500', 'text-white', 'border-blue-500'] : ['text-blue-500']"
+        @click="rvPositive = 1"
+      >
         <svg
           v-if="rvPositive === 1"
           width="20"
@@ -108,7 +174,11 @@ await loadMyReview()
         ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         추천
       </button>
-      <button class="btn btn-sm w-32 btn-outline border-red-500 text-red-500 hover:bg-red-500 hover:text-white hover:border-red-500" @click="rvPositive = 0">
+      <button
+        class="btn btn-sm w-32 btn-outline border-red-500 hover:bg-red-500 hover:text-white hover:border-red-500"
+        :class="rvPositive === 0 ? ['bg-red-500', 'text-white', 'border-red-500'] : ['text-red-500']"
+        @click="rvPositive = 0"
+      >
         <svg
           v-if="rvPositive === 0"
           width="20"
@@ -121,13 +191,13 @@ await loadMyReview()
         비추천
       </button>
     </div>
-    <div class="flex">
-      <div class="flex flex-1 items-center space-x-2">
-        <button v-if="!myReview" class="btn btn-md w-full btn-primary" :disabled="!isReadyToSubmit" @click="create">
-          게시하기
+    <div class="flex space-x-2">
+      <div class="flex flex-1 items-center">
+        <button v-if="!myReview" class="btn btn-md w-full btn-primary" :class="{loading: isRunning}" :disabled="!isReadyToSubmit" @click="create">
+          {{ !isRunning ? '게시하기' : '잠시만 기다려주세요' }}
         </button>
-        <button v-else class="btn btn-md w-full btn-primary" :disabled="!isReadyToSubmit" @click="update">
-          수정하기
+        <button v-else class="btn btn-md w-full btn-primary" :class="{loading: isRunning}" :disabled="!isReadyToSubmit" @click="update">
+          {{ !isRunning ? '수정하기' : '잠시만 기다려주세요' }}
         </button>
       </div>
     </div>
