a522d8a5d1ee0e6c27083ae89aab032ee24580c0
scissorstail <scissorstail@gmail.com>
2022-06-04 21:11:28 +0900

    review update 기능 수정

--- backend/api/src/api/user/v1/review/update.js
+++ backend/api/src/api/user/v1/review/update.js
@@ -1,8 +1,10 @@
 module.exports = async ({ sql, params }) => {
   const query = await sql`
   UPDATE review SET
-    rv_positive = ${params.rv_positive}
-    , rv_content =  ${params.rv_content}
+    rv_positive = COALESCE(${params.rv_positive}, rv_positive)
+    , rv_content = COALESCE(${params.rv_content}, rv_content)
+    , state = COALESCE(${params.state}, state)
+    , updated_dt = now()
   WHERE
     rv_no = ${params.rv_no}
     AND user_no = ${params.user_no}
