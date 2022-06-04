c8aa5019d67ec8fe0644a9c23d70f6c91f920d1f
scissorstail <scissorstail@gmail.com>
2022-06-05 00:52:28 +0900

    프로필 쿼리 오류 수정

--- backend/api/src/api/user/v1/users/select.js
+++ backend/api/src/api/user/v1/users/select.js
@@ -3,10 +3,10 @@ module.exports = async ({ sql, params }) => {
   WITH reviews AS (
     SELECT
       u.user_no
-      , (count(r.rv_no) FILTER(WHERE r.rv_positive = 1))::int4 AS count_positive
-      , (count(r.rv_no))::int4 AS count_total
-      , (count(r.rv_no) FILTER(WHERE r.rv_positive = 1 AND r.created_dt >= now() - INTERVAL '30 days'))::int4 AS count_positive_recent
-      , (count(r.rv_no) FILTER(WHERE r.created_dt >= now() - INTERVAL '30 days'))::int4 AS count_total_recent
+      , count(r.rv_no) FILTER(WHERE r.rv_positive = 1) AS count_positive
+      , count(r.rv_no) AS count_total
+      , count(r.rv_no) FILTER(WHERE r.rv_positive = 1 AND r.created_dt >= now() - INTERVAL '30 days') AS count_positive_recent
+      , count(r.rv_no) FILTER(WHERE r.created_dt >= now() - INTERVAL '30 days') AS count_total_recent
     FROM review r
     JOIN users u ON r.rv_user_no = u.user_no 
     WHERE
@@ -20,13 +20,13 @@ module.exports = async ({ sql, params }) => {
     , u.user_name
     , u.user_nick
     , u.user_provider_info -> 'photos' -> 0 ->> 'value' AS user_profile_image_url
-    , reviews.count_positive
-    , reviews.count_total
-    , reviews.count_positive_recent
-    , reviews.count_total_recent
+    , COALESCE(reviews.count_positive, 0)::int4 AS count_positive
+    , COALESCE(reviews.count_total, 0)::int4 AS count_total
+    , COALESCE(reviews.count_positive_recent, 0)::int4 AS count_positive_recent
+    , COALESCE(reviews.count_total_recent, 0)::int4 AS count_total_recent
   FROM
     users u
-  JOIN reviews ON reviews.user_no = u.user_no  
+  LEFT JOIN reviews ON reviews.user_no = u.user_no  
   WHERE
     u.user_name = ${params.user_name}
     AND u.state = 1
