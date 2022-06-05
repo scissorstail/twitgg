25e4e4e07f6a5ceed58430d0bd591ef4197de07a
scissorstail <scissorstail@gmail.com>
2022-06-05 17:57:08 +0900

    local 인증 제거, 쿠키 maxAge 설정

--- backend/api/src/api/user/v1/auth/index.js
+++ backend/api/src/api/user/v1/auth/index.js
@@ -1,21 +1,8 @@
+const ms = require('ms');
+
 const config = require('@/config');
 const passport = require('@/api/user/v1/auth/passport');
 const { handle } = require('@/middlewares/routine-handler');
-
-async function login(req, res) {
-  // 로그인 실패 시
-  if (req.user.error) {
-    delete req.user.error;
-    return res.status(500).json(req.user);
-  }
-
-  // 로그인 성공 시
-  const { token } = req.user;
-  res.cookie(config.auth.jwtTokenName, token, {
-    httpOnly: false,
-  });
-  return res.json({ token });
-}
 
 async function loginTwitter(req, res) {
   // 로그인 실패 시
@@ -28,6 +15,7 @@ async function loginTwitter(req, res) {
   const { token } = req.user;
   res.cookie(config.auth.jwtTokenName, token, {
     httpOnly: false,
+    maxAge: ms(config.auth.jwtExpireUser),
   });
   return res.redirect(`${config.app.webUrl}`);
 }
@@ -36,9 +24,6 @@ module.exports = (app) => {
   // twitter
   app.get('/login/twitter', passport.authenticate('user.twitter', { session: false, scope: ['tweet.read', 'tweet.write', 'users.read'] })); // 트위터 로그인
   app.get('/login/twitter/callback', passport.authenticate('user.twitter', { session: false, failureRedirect: `${config.app.webUrl}/social-redirect/twitter?success=false&state=${-4}` }), loginTwitter); // 트위터 로그인 콜백
-
-  // local
-  app.post('/login', [passport.authenticate('user.local', { session: false })], login);
 
   app.group([passport.authenticate('user.jwt', { session: false })], (router) => {
     router.get('/profile', handle(require('./profile')));
--- backend/api/src/api/user/v1/auth/passport.js
+++ backend/api/src/api/user/v1/auth/passport.js
@@ -10,60 +10,7 @@ const bcrypt = require('bcrypt');
 const ExtractJWT = passportJWT.ExtractJwt;
 
 const JWTStrategy = passportJWT.Strategy;
-const LocalStrategy = require('passport-local').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
-
-// Local Strategy
-passport.use('user.local', new LocalStrategy(
-  {
-    usernameField: 'user_id',
-    passwordField: 'user_pw',
-    session: false, // 세션 사용안함
-  },
-  async (userId, userPw, done) => {
-    try {
-      // userId, userPw로 사용자 인증 확인. 해당 값이 없으면 400 리턴
-      const query = await sql`
-      SELECT
-        user_no
-        , user_pw
-        , state
-      FROM users
-      WHERE
-        user_id = ${userId}
-      `;
-
-      if (query.length === 0) {
-        // 없는 사용자
-        return done(null, { error: true, state: -1, msg: 'User not found' }, {});
-      }
-      const user = query[0];
-
-      // const check = await bcrypt.compare(userPw, user.userPw);
-      // if (!check) {
-      //   // 비밀번호 틀림
-      //   return done(null, { error: true, state: -2, msg: 'Incorrect email or password' }, {});
-      // }
-
-      if (user.state === 0) {
-        // 비활성화된 사용자
-        return done(null, { error: true, state: -3, msg: 'Disabled user' }, {});
-      }
-
-      // JWT 토큰 생성
-      const token = jwt.sign({ user_no: user.user_no }, config.auth.jwtSecretUser, {
-        expiresIn: config.auth.jwtExpireUser, // https://github.com/zeit/ms
-      });
-
-      // 로그인 체크 성공
-      return done(null, { token }, {});
-    } catch (e) {
-      // 로그인 확인 중 에러 발생 시
-      console.error(e);
-      return done(null, { error: true, state: 0, msg: 'Internal Error' }, {});
-    }
-  },
-));
 
 // 쿠키 인증 방식 사용 시
 // const cookieExtractor = (cookieName) => (req) => {
